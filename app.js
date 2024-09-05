d3494e02d39a242fc0bfcf31ad4c26b28fcc5fbc
GyuTae Kim <supxyrho@gmail.com>
2024-09-05 16:28:31 +0900

    Update

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const R = require("ramda");
 const { of, tap, interval, map } = require("rxjs");
 
 const { step } = require("./rx-js-step");
+const { on } = require("events");
 
-// of("test 1 - onBefore, onAfter")
+ 
+// of("___A___")
 //   .pipe(
 //     tap(() => console.log("tap")),
 //     step({
@@ -21,23 +23,62 @@ const { step } = require("./rx-js-step");
 //   )
 //   .subscribe();
 
-  of("test 2 - onError")
-  .pipe(
-    step({
-      id: "test",
-      operator: (source$)=> source$.pipe(
-        map(()=> {throw new Error("error_____")}),
-      ),
-      sideEffects: [
-        {
-          onError:(id, error, value)=> console.log("onError", id,  value, error),
-        },
-      ],
-      interceptors: [],
-    })
-  )
-  .subscribe({
-    next: (value) => console.log("next subscribe", value),
-    error: (error) => console.log("error subscribe", error),
-    complete: () => console.log("complete subscribe"),
-  });
+
+  // of("____B_____")
+  // .pipe(
+  //   step({
+  //     id: "test",
+  //     operator: (source$)=> source$.pipe(
+  //       map(()=> {throw new Error("error 1_____")}),
+  //     ),
+  //     sideEffects: [
+  //       {
+  //         onError:(id, error, value)=> console.log("onError 1 ", id,  value, error),
+  //       },
+  //     ],
+  //     interceptors: [],
+  //   }),
+  //   step({
+  //     id: "test-2",
+  //     operator: (source$)=> source$.pipe(
+  //       map(()=> {throw new Error("error 2_____")}),
+  //     ),
+  //     // interceptors: [
+  //     //   {onBefore: (value)=> value, onAfter: (value)=> value, onError: (error)=> error},
+  //     // ],
+  //     // skipUpstreamErrorOnSideEffects:true,
+  //     interceptors: [],
+  //   })
+  // )
+  // .subscribe({
+  //   next: (value) => console.log("next subscribe", value),
+  //   error: (error) => console.log("error subscribe", error),
+  //   complete: () => console.log("complete subscribe"),
+  // });
+
+    // skipUpstreamErrorOnSideEffects true 테스트
+    of("____C_____")
+    .pipe(
+      step({
+        id: "test",
+        operator: (source$)=> source$.pipe(
+          map(()=> {throw new Error("error 1_____")}),
+        ),
+        interceptor: {
+            onBefore: (id, value)=> {
+              console.log('__onBefore', id, value);
+              return 'd'
+            }, 
+            onAfter: (id, value)=> console.log('__onAfter', id, value),
+            onError: (id, error, acc)=> { 
+              console.log('__onError', id, error, acc)
+              return 'error-d'
+            },
+        }
+      }),
+    )
+    .subscribe({
+      next: (value) => console.log("next subscribe", value),
+      error: (error) => console.log("error subscribe", error),
+      complete: () => console.log("complete subscribe"),
+    });
